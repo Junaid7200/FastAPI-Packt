3efc15d628ab521f151625607a33cdf16084db4b
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-30 03:35:52 +0500

    feat: setup scalar openapi doc endpoint

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
-
+from scalar_fastapi import get_scalar_api_reference
 app = FastAPI()
 
 @app.get("/")
 def get_shipment():
     return {"message": "Shipment service is running."}
 
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return(get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="JJ API Documentation"
+    ))
