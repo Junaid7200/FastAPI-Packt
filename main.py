48301a84fb4d70c82295907b9492447db0fba246
Junaid7200 <junaidjaffery1@gmail.com>
2026-01-31 11:54:03 +0500

    feat: code a fastapi route with route params

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 app = FastAPI()
 
+
+
 @app.get("/")
 def get_shipment():
     return {"message": "Shipment service is running."}
+
+
+@app.get("/{id}")
+def get_shipment_by_id(id: int):
+    return {"message": f"Shipment service is running for the id: {id}."}
 
 
 @app.get("/scalar", include_in_schema=False)
