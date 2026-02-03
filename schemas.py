a2f37577e76888488d86b6bcccad2f5de85336a4
Junaid7200 <junaidjaffery1@gmail.com>
2026-02-03 06:59:02 +0500

    feat: code an enum for the status field

--- schemas.py
+++ schemas.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel, Field
+from enum import Enum
 
+
+class ShipmentStatus(str, Enum):
+    placed= "placed"
+    in_transit="in_transit"
+    delivered="delivered"
 
 class shipment(BaseModel):
     content: str = Field(description="this field will describe the contents of the shipment in string", max_length=100)
     weight: float = Field(lt=25, ge=1)
-    status: str
+    status: ShipmentStatus
     destination: int | None = Field(default=None)
